d45e59e0528cd299ed9a6b95f3ff3a2c460013ba
Guy Barker (CELA) <guybark@microsoft.com>
2020-12-16 11:36:52 -0800

    Add project files.

---
+++ WinForms_Progress/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace WinForms_Progress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(666, 395);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(122, 43);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 381);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(131, 57);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonClose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ WinForms_Progress/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms_Progress
+{
+    public partial class Form1 : Form
+    {
+        private MyProgressBar myProgressBar;
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create some control that we'll consider to be a progress bar.
+            myProgressBar = new MyProgressBar();
+            myProgressBar.Location = new Point(10, 10);
+            myProgressBar.Size = new Size(100, 100);
+            myProgressBar.BackColor = Color.Green;
+            this.Controls.Add(myProgressBar);
+
+            myProgressBar.CurrentValue = 0;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Update the value on our custom progress bar every couple of seconds.
+            if (timer == null)
+            {
+                myProgressBar.CurrentValue = 0;
+
+                timer = new Timer();
+                timer.Interval = 2000;
+                timer.Tick += Timer_Tick;
+
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update the value of the progress bar now, limiting it to 100.
+            myProgressBar.CurrentValue = Math.Min(100, myProgressBar.CurrentValue + 10);
+
+            if (myProgressBar.CurrentValue >= 100)
+            {
+                timer.Stop();
+                timer = null;
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class MyProgressBar : Control
+    {
+        private int currentValue;
+        public int CurrentValue 
+        {
+            get
+            {
+                return currentValue;
+            }
+            set
+            {
+                currentValue = value;
+
+                // Let any AT listening know of the value change.
+
+                // Note: Ultimately we expect WinForms to raise an old MSAA EVENT_OBJECT_VALUECHANGE event through NotifyWinEvent(), 
+                // passing in a child id of CHILDID_SELF, (ie zero). This means the windowed control itself is raising the event, and 
+                // not some windowless element beneath that control. In one of the implmentations of AccessibilityNotifyClients() in 
+                // https://referencesource.microsoft.com/#System.Windows.Forms/winforms/Managed/System/WinForms/Control.cs
+                // it shows NotifyWinEvent() being called with the supplied childId plus 1. So if we ultimately want NotifyWinEvent()
+                // to be called with CHILDID_SELF, we must pass in -1 here.
+
+                this.AccessibilityNotifyClients(AccessibleEvents.ValueChange, -1);
+            }
+        }
+
+        protected override AccessibleObject CreateAccessibilityInstance()
+        {
+            return new MyProgressBarAccessibleObject(this);
+        }
+
+        public class MyProgressBarAccessibleObject : ControlAccessibleObject
+        {
+            private MyProgressBar owner;
+
+            public MyProgressBarAccessibleObject(MyProgressBar owner) : base(owner)
+            {
+                this.owner = owner;
+            }
+
+            public override string Name
+            {
+                get
+                {
+                    return "Install"; // Some string for a test.
+                }
+            }
+
+            public override AccessibleRole Role
+            {
+                get
+                {
+                    return AccessibleRole.ProgressBar;
+                }
+            }
+
+            public override string Value 
+            {
+                get
+                {
+                    return owner.CurrentValue.ToString();
+                }
+            }
+        }
+    }
+}
